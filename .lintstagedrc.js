6df4b7102a757c0fc551b6f2d6681305e4575ea2
Felipe Bambirra <lipex360@gmail.com>
2023-09-23 20:58:47 -0300

    build: initial tests started

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,7 +2,7 @@ module.exports = {
   "*.{js,jsx,ts,tsx,json,css,html}": ["prettier --write"],
   "*.{js,ts,tsx}": [
     "yarn eslint . --ext .ts,.tsx --fix",
-    // 'jest --bail --findRelatedTests --passWithNoTests',
+    "jest --bail --findRelatedTests --passWithNoTests",
   ],
   "**/*.ts": () => "yarn typecheck",
 };
