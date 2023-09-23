d5530ca6a76a617a416f8b98507af1754f3eba34
Felipe Bambirra <lipex360@gmail.com>
2023-09-23 20:27:10 -0300

    build: jest tests added

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'jest'
+
+export default <Config>{
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts(x)?'],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  modulePaths: ['<rootDir>/src/'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+  },
+}
